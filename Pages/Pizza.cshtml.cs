1109a0bf4086a9ec28ef8f59cc2503d553cc380c
primula4 <6180817@cegep-heritage.qc.ca>
2022-03-31 14:01:41 -0400

    add files

---
+++ Pages/Pizza.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesPizaa.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
